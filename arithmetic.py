58f0cc4ebf67e0ce02937113ff6affd8809ce008
Hackbright Student <info@hackbrightacademy.com>
2015-09-23 17:07:23 -0700

    first commit

---
+++ arithmetic.py
@@ -0,0 +1,31 @@
+def add(num1, num2):
+    """Returns the sum of the two input integers"""
+    return int(num1) + int(num2)
+
+def subtract(num1, num2):
+    """Returns the second number subtracted from the first"""
+    return int(num1) - int(num2)
+
+def multiply(num1, num2):
+    """ Multiplies the two inputs together"""
+    return int(num1) * int(num2)
+
+def divide(num1, num2):
+    """ Divides the first input by the second, returning a floating point"""
+    return float(num1) / float(num2)
+
+def square(num1):
+    """Returns the square of the input"""
+    return int(num1) * int(num1)
+
+def cube(num1):
+    """Returns the cube of the input"""
+    return int(num1) * int(num1) * int(num1)
+
+def power(num1, num2):
+    """Raises the first integer to the power of the second integer and returns the value"""
+    return int(num1) ** int(num2)
+
+def mod(num1, num2):
+    """Returns the remainder when the first integer is divided by the second integer"""
+    return int(num1) % int(num2)
---
+++ calculator.py
@@ -0,0 +1,21 @@
+"""
+calculator.py
+
+Using our arithmetic.py file from Exercise02, create the
+calculator program yourself in this file.
+"""
+
+from arithmetic import *
+
+
+# Your code goes here
+user_input = raw_input("> ")
+
+def calc2(user_input):
+    tokens = user_input.split()
+    
+    if tokens[0] == "+":
+        result = add(tokens[1], tokens[2])
+        print result
+
+calc2(user_input)
